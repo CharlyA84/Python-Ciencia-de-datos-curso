e6c4ed49a807609f61846023c84c95c3faae5be4
CharlyA84 <antonio.torres84@icloud.com>
2024-12-16 23:51:41 -0600

    Add files via upload
    Condiconales

---
+++ #Condicionbes mas complejas.py
@@ -0,0 +1,9 @@
+#Condicionbes mas complejas
+#asiganacion de valor inicial
+x = 8
+print (x > 4 or x > 12)
+print (x < 4 or x> 12)
+print (x > 4 and x >12)
+print (x > 4 and x < 12 )
+print (not(x != 8))
+print (4 <= ValueError <= 12)
